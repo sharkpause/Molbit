dd5f524697363a92e5b9dc250c4e903a9e34536c
sharkpause <carlmabfredishere@gmail.com>
2025-12-23 01:40:39 +0700

    feat: made to read and write to file

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,34 @@ mod lexer;
 mod parser;
 mod codegen;
 
+use std::{env, fs};
+
 use crate::token::print_token;
 use crate::lexer::Lexer;
 use crate::parser::Parser;
 use crate::codegen::codegen;
 
+fn read_file(path: &String) -> String {
+    let source_code =
+        fs::read_to_string(path).expect("Failed to read source code");
+
+    return source_code;
+}
+
+fn write_file(path: String, contents: &String) {
+    fs::write(path, contents).expect("Failed to write assembly");
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Incorrect usage");
+        return;
+    }
+
     let mut lexer = Lexer {
-        input: String::from("return 67;"),
+        input: read_file(&args[1]),
         index: 0
     };
 
@@ -31,5 +51,5 @@ fn main() {
     println!("\nStatements:\n{:?}", statements);
 
     let output = codegen(statements);
-    println!("\nAssembly output:\n{}", output);
+    write_file(String::from("out.asm"), &output);
 }
