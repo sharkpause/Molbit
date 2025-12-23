2ea550675504759153571a67962a352abb2bec9d
sharkpause <carlmabfredishere@gmail.com>
2025-12-23 17:45:20 +0700

    feat: extended expression codegeneration

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,4 +1,4 @@
-use crate::parser::{Expression, Statement,TopLevel};
+use crate::parser::{Expression, Operator, Statement, TopLevel};
 
 #[derive(Debug)]
 pub enum CodegenError {
@@ -41,8 +41,8 @@ fn codegen_statement(statement: Statement) -> Result<String, CodegenError> {
     let mut output = String::new();
 
     match statement {
-        Statement::Return(Expression::IntLiteral(value)) => {
-            output.push_str(&format!("    mov rax, {}\n", value));
+        Statement::Return(expression) => {
+            output.push_str(&codegen_expression(expression)?);
             output.push_str("    ret\n");
         
             return Ok(output);
@@ -51,4 +51,49 @@ fn codegen_statement(statement: Statement) -> Result<String, CodegenError> {
             return Err(CodegenError::GenericError);
         }
     }
+}
+
+fn codegen_expression(expression: Expression) -> Result<String, CodegenError> {
+    let mut output = String::new();
+
+    match expression {
+        Expression::IntLiteral(value) => {
+            output.push_str(&format!("    mov rax, {}\n", value));
+        },
+        Expression::UnaryOp(operator, inner) => {
+            output.push_str(&codegen_expression(*inner)?);
+            output.push_str("    neg rax\n");
+        },
+        Expression::BinaryOp(lhs,operator ,rhs ) => {
+            let left = codegen_expression(*lhs)?;
+            output.push_str(&left);
+            output.push_str("    push rax\n");
+
+            let right = codegen_expression(*rhs)?;
+            output.push_str(&right);
+            output.push_str("    pop rbx\n");
+
+            match operator {
+                Operator::Add => {
+                    output.push_str("    add rbx, rax\n");
+                    output.push_str("    mov rax, rbx\n");
+                },
+                Operator::Subtract => {
+                    output.push_str("    sub rbx, rax\n");
+                    output.push_str("    mov rax, rbx\n");
+                },
+                Operator::Multiply => {
+                    output.push_str("    imul rbx, rax\n");
+                    output.push_str("    mov rax, rbx\n");
+                },
+                Operator::Divide => {
+                    output.push_str("    xchg rax, rbx\n");
+                    output.push_str("    xor rdx, rdx\n");
+                    output.push_str("    idiv rbx\n");
+                }
+            }
+        }
+    }
+
+    return Ok(output);
 }
